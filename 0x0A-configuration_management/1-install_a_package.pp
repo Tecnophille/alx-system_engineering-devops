b9529146e7bb1d090029f8b12138fb9cd0268cb7
Oliver Samuel <104834870+Tecnophille@users.noreply.github.com>
2022-12-12 10:00:41 +0100

    File installation(pip)

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
-# Installs puppet-lint, version 2.1.1
+# Using Puppet, install flask from pip3 
 
-package { 'puppet-lint':
+package { 'flask':
   ensure   => '2.1.0',
-  provider => 'gem',
+  provider => 'pip3'
 }
